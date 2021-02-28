f044fa075604b19055bd77f23de8be412e028a32
Emerson Solis <>
2021-02-27 19:12:09 -0500

    Organizando clases

---
+++ src/proyectogrupalfinal/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogrupalfinal;
+
+/**
+ *
+ * @author User
+ */
+public  class  Persona {
+    
+   private String nombre;
+   private String apellido;
+   private int NumeroIdentidad;
+   private double factura;
+
+    public Persona(String nombre, String apellido, int NumeroIdentidad, double factura) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.NumeroIdentidad = NumeroIdentidad;
+        this.factura = factura;
+    }
+
+    //public abstract void CalcularFactura();
+    
+ 
+    
+}
