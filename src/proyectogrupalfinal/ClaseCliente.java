f12dd40f4c5dba13eef166a51ab79a8bb5376b70
David Andres <David Andres@David_Andres>
2021-02-28 11:50:15 -0500

    implemetancion de herencia de la clasePersona a las subcalses cliente y empleado; implentacion de get , set y metodo Tostring

--- src/proyectogrupalfinal/ClaseCliente.java
+++ src/proyectogrupalfinal/ClaseCliente.java
@@ -9,16 +9,38 @@ package proyectogrupalfinal;
  *
  * @author User
  */
-public class ClaseCliente {
+public  class ClaseCliente extends ClasePersona{
 
     private int edad;
     private double precioFinal;
+
+    public ClaseCliente(int edad, double precioFinal, String nombre, String apellido, int NumeroIdentidad, double factura) {
+        super(nombre, apellido, NumeroIdentidad, factura);
+        this.edad = edad;
+        this.precioFinal = precioFinal;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
     
-    
-    
-    /**
-     *
-     */
- 
+    @Override
+    public String toString() {
+        return "Datos del Cliente es :" +super.toString()+"edad=" + edad + ", precioFinal=" + precioFinal + '}';
+    }
+
     
 }
