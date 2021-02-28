f0c6d3b6e4c39f329640ce0b9553576c8ce48365
Emerson Solis <>
2021-02-27 19:11:39 -0500

    Organizando clase automovil

---
+++ src/proyectogrupalfinal/Automovil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogrupalfinal;
+
+/**
+ *
+ * @author User
+ */
+public class Automovil {
+    
+    private String modelo;
+    private String tipo;
+    private int cantidad;
+
+    @Override
+    public String toString() {
+        return "ClaseAutomovil{" + "modelo=" + modelo + ", tipo=" + tipo + ", cantidad=" + cantidad + '}';
+    }
+    
+    
+    
+    
+}
