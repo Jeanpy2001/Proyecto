50d395ad1f65fc65a0f462e917130e45702c78f7
Emerson Solis <>
2021-03-01 02:01:36 -0500

    Agregando get y set a la clase Automovil

--- src/proyectogrupalfinal/Automovil.java
+++ src/proyectogrupalfinal/Automovil.java
@@ -19,8 +19,29 @@ public class Automovil {
     public String toString() {
         return "ClaseAutomovil{" + "modelo=" + modelo + ", tipo=" + tipo + ", cantidad=" + cantidad + '}';
     }
-    
-    
-    
-    
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+      
 }
