e5e43252ff23644def7a0c12a9284107daf0134f
Syslan <Syslan@192.168.56.1>
2021-03-04 09:40:04 -0500

    cambios en la clase Persona

--- src/proyectogrupalfinal/ClasePersona.java
+++ src/proyectogrupalfinal/ClasePersona.java
@@ -14,13 +14,13 @@ public  class  ClasePersona  {
    private String nombre;
    private String apellido;
    private int NumeroIdentidad;
-   private double factura;
+   
 
-    public ClasePersona(String nombre, String apellido, int NumeroIdentidad, double factura) {
+    public ClasePersona(String nombre, String apellido, int NumeroIdentidad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.NumeroIdentidad = NumeroIdentidad;
-        this.factura = factura;
+        
     }
 
     //public abstract void CalcularFactura();
