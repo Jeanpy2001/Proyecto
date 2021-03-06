438cce472e53b08a3bb03f8c093e756134f12de5
David Andres <David Andres@David_Andres>
2021-03-06 17:31:31 -0500

    agregar datos de vehiculos

--- src/proyectogrupalfinal/Prototipos.java
+++ src/proyectogrupalfinal/Prototipos.java
@@ -14,6 +14,67 @@ public class Prototipos {
     
     
     //ESTA CLASE SIRVE COMO BORRADOR POR SI DESEAN PLANTEAR UNA IDEA
+  /*  
+    Mercedes Benz 
+
+    
+    
+    
+     Modelo:  Mercedes Benzs Clase B B 180D
+Tipo: SUV crossover
+Marca: Mercedes Benz 
+Precio: 28.462
+Año: 2013
+Kilometraje: 190 km/h.
+
+    Modelo:  Mercedes Benzs GLA
+Tipo:  SUV compacto
+Marca: Mercedes Benz
+Precio: 40.800
+Año: 2014
+Kilometraje: 210 km/h
+
+    
+    
+    
+    
+    
+    BMW
+
+    Modelo: BMW Serie 7
+Tipo: sedán
+Marca: BMW
+Precio:  101.400
+Año: 2019
+Kilometraje: 250 km/h
+
+    Modelo:  BMW GRT M3
+Tipo: Coupé
+Marca: BMW
+Precio: 55.000
+Año: 2006
+Kilometraje: 260 km/h
+
+
+
+Hyundai
+    Modelo:  Accent
+Tipo:  Sedán
+Marca: Hyundai
+Precio: 45,900
+Año: 2018
+Kilometraje: 190 km/h
+
+   Modelo:   Verna
+Tipo:  Subcompacto
+Marca: Hyundai
+Precio: 18.250 
+Año: 2020
+Kilometraje: 170 km/h
+
+
+    
+    
     
     
     /*
