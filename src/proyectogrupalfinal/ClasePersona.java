54aa8dcd0e0e707a7c0a50202e5d300a7d682060
Emerson Solis <>
2021-03-10 02:51:53 -0500

    Agregando codigo para validar la ID

--- src/proyectogrupalfinal/ClasePersona.java
+++ src/proyectogrupalfinal/ClasePersona.java
@@ -13,10 +13,10 @@ public  class  ClasePersona  {
     
    private String nombre;
    private String apellido;
-   private int cedula;
+   private String cedula;
    
 
-    public ClasePersona(String nombre, String apellido, int NumeroIdentidad) {
+    public ClasePersona(String nombre, String apellido, String NumeroIdentidad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.cedula = NumeroIdentidad;
@@ -39,11 +39,11 @@ public  class  ClasePersona  {
         this.apellido = apellido;
     }
 
-    public int getCedula() {
+    public String getCedula() {
         return cedula;
     }
 
-    public void setCedula(int cedula) {
+    public void setCedula(String cedula) {
         this.cedula = cedula;
     }
     
@@ -54,6 +54,41 @@ public  class  ClasePersona  {
         return "ClasePersona{" + "nombre=" + nombre + ", apellido=" + apellido + ", NumeroIdentidad=" + cedula +  '}';
     }
     
+    public boolean esValidaLaID(){
+        int verificacion=0;
+        int[] num = new int[10];
+        String cedula;
+        cedula= this.cedula;
+        if(cedula.length()==10){
+            char[] id= cedula.toCharArray();
+            for (int i=0 ; i<10 ; i++){
+               num[i] = Character.getNumericValue(id[i]);  
+            }                   
+            int suma=0;
+            int i=0,j=1;
+            int sum1, sum2;
+            while(i<9){
+                sum1 = num[i]*2;
+                if(sum1>=10){
+                   sum1=sum1-9;
+                }
+                suma=suma+sum1;
+                i=i+2;          
+            }
+            while(j<8){
+                sum2 = num[j]*1;
+                if(sum2>=10){
+                   sum2=sum2-9;
+                }
+                suma=suma+sum2;
+                j=j+2;          
+            }  
+        
+        verificacion = 10 - (suma%10);
+        
+    }
+    return verificacion==num[9];
+    }    
  
     
 }
