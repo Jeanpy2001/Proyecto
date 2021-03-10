b8bb16b2bbb626ba5554afb0f5969c0ea4e650cc
Emerson Solis <>
2021-03-10 02:18:46 -0500

    Eliminando el código de validación de fecha

--- src/proyectogrupalfinal/GuiProyectoFinal.java
+++ src/proyectogrupalfinal/GuiProyectoFinal.java
@@ -525,13 +525,7 @@ public class GuiProyectoFinal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
     
-    private boolean fechaEsValida(){
-        boolean resultado = txtFecha.getText().matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)"
-                + "(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?"
-                + "(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)"
-                + "(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
-        return resultado;
-    }
+
     
     private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
         // TODO add your handling code here:
