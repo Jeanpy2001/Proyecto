084cdb12528bd5ee57c5dc9963a8729ac401f3a4
Syslan <Syslan@192.168.56.1>
2021-03-12 18:48:22 -0500

    cambios en clase Fecha

--- src/proyectogrupalfinal/Fecha.java
+++ src/proyectogrupalfinal/Fecha.java
@@ -12,7 +12,7 @@ package proyectogrupalfinal;
 public class Fecha {
     private String fecha;
     
-    private boolean fechaEsValida(){
+    public boolean fechaEsValida(){
         boolean resultado = this.fecha.matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)"
                 + "(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?"
                 + "(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)"
